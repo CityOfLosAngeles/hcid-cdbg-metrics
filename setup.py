a167c45f2a0ad1db5084b218eff99ac567c3ee5b
tiffanychu90 <tiffany.chu@lacity.org>
2019-09-19 21:09:00 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Evaluate Community Development Block Grants',
+    author='Citywide Data Science and Predictive Analytics',
+    license='Apache',
+)
