43fb53682e486315c0a615fc394712e55439dd57
tiffanychu90 <tiffany.chu@lacity.org>
2020-08-07 14:04:16 -0700

    Download block group Census data

--- src/A3_download_census_income.R
+++ src/A3_download_census_income.R
@@ -4,12 +4,12 @@
 
 library(tidycensus)
 library(tidyverse)
-library(censusapi)
+#library(censusapi)
 Sys.getenv("CENSUS_API_KEY")
 Sys.getenv("CENSUS_KEY")
 
 # Set working directory
-#setwd("GitHub/hcid-cdbg-metrics")
+setwd("GitHub/hcid-cdbg-metrics")
 
 ## Load years
 tract_years <- list(2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017)
@@ -176,3 +176,81 @@ inc2 = do.call(rbind, inc2_list)
 
 write_csv(inc2, "data/Census/income_range_hh_tract.csv")
 print('Saved data/Census/income_range_hh_tract.csv')
+
+
+#------------------------------------------------------------------#
+## Household Income Ranges by Block Group -- just 2017
+#------------------------------------------------------------------#
+print('Download income ranges by block group (B19001) 2017')
+income_range = list()
+
+for (y in 2017) {
+  var <- load_variables(y, "acs5", cache = TRUE)
+  columns <- var %>% filter(str_detect(name, "B19001_"))
+  
+  ca <- get_acs(geography = "block group", year = y, variables = columns$name,
+                state = "CA", survey = "acs5", geometry = FALSE)
+  
+  la <- ca %>% filter((str_detect(variable, "001$") |
+                         str_detect(variable, "002$") |
+                         str_detect(variable, "003$") |
+                         str_detect(variable, "004$") |
+                         str_detect(variable, "005$") |
+                         str_detect(variable, "006$") |
+                         str_detect(variable, "007$") |
+                         str_detect(variable, "008$") |
+                         str_detect(variable, "009$") |
+                         str_detect(variable, "010$") |
+                         str_detect(variable, "011$") |
+                         str_detect(variable, "012$") |
+                         str_detect(variable, "013$") |
+                         str_detect(variable, "014$") |
+                         str_detect(variable, "015$") |
+                         str_detect(variable, "016$") |
+                         str_detect(variable, "017$")) &
+                        str_detect(GEOID, "^06037")
+  )
+  
+  la$year <- y
+  income_range[[y]] <- la
+  
+}
+
+
+# Append all the years into a df
+print('Append income ranges df')
+income_range2 = do.call(rbind, income_range)
+
+write_csv(income_range2, "data/Census/income_range_hh_blockgroup.csv")
+print('Saved data/Census/income_range_hh_blockgroup.csv')
+
+
+#------------------------------------------------------------------#
+## Median Household Income by Block Group -- just 2017
+#------------------------------------------------------------------#
+print('Download median hh income by block group (B19013) 2017')
+medincome = list()
+
+for (y in 2017) {
+  var <- load_variables(y, "acs5", cache = TRUE)
+  columns <- var %>% filter(str_detect(name, "B19013_"))
+  
+  ca <- get_acs(geography = "block group", year = y, variables = columns$name,
+                state = "CA", survey = "acs5", geometry = FALSE)
+  
+  la <- ca %>% filter((str_detect(variable, "001$") &
+                        str_detect(GEOID, "^06037"))
+  )
+  
+  la$year <- y
+  medincome[[y]] <- la
+  
+}
+
+
+# Append all the years into a df
+print('Append income ranges df')
+medincome2 = do.call(rbind, medincome)
+
+write_csv(medincome2, "data/Census/medianincome_blockgroup.csv")
+print('Saved data/Census/medianincome_blockgroup.csv')
